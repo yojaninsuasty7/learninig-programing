f105fa7e872730d666df7170fd1a58a08f42919a
yojaninsuasty7 <yojaninsuasty7@gmail.com>
2026-05-19 09:40:44 -0500

    prueba

---
+++ sistema_registro.py
@@ -0,0 +1,36 @@
+import os
+
+registerclient = []
+registerproduct = []
+listclients = []
+listproduct = []
+searchclient = []
+searchproduct = []
+updateclient = []
+updateproduct = []
+delteclient = []
+dleteproduct = []
+exit = []
+
+
+
+
+
+
+
+
+
+def main_menu():
+    print("### MAIN MENU ###")
+    print("[1]. register client")
+    print("[2]. register product")
+    print("[3]. list clients")
+    print("[4]. list products")
+    print("[5]. search client")
+    print("[6]. search product")
+    print("[7]. update client")
+    print("[8]. update product")
+    print("[9]. delte client")
+    print("[10].dlete product")
+    print("[11]. exit")
+    print("press any option: ")
